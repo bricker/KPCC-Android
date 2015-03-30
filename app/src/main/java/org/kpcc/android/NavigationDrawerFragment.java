0bf3f696e9341e12ef4c8200156c29817a1536f6
Bryan Ricker <bryancricker@gmail.com>
2015-03-29 23:02:28 -0700

    63: Remove user learned drawer feature

--- app/src/main/java/org/kpcc/android/NavigationDrawerFragment.java
+++ app/src/main/java/org/kpcc/android/NavigationDrawerFragment.java
@@ -26,7 +26,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class NavigationDrawerFragment extends Fragment {
     private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer_position";
-    private static final String PREF_USER_LEARNED_DRAWER = "navigation_drawer_learned";
     private final AtomicBoolean mDoFragmentTransaction = new AtomicBoolean(false);
     private AnalyticsManager mAnalyticsManager;
     private ActionBarDrawerToggle mDrawerToggle;
@@ -35,7 +34,6 @@ public class NavigationDrawerFragment extends Fragment {
     private View mFragmentContainerView;
     private int mCurrentSelectedPosition = 0;
     private boolean mFromSavedInstanceState;
-    private boolean mUserLearnedDrawer;
     private Float mWrapperAlpha = null;
 
     @Override
@@ -44,11 +42,6 @@ public class NavigationDrawerFragment extends Fragment {
         setRetainInstance(true);
 
         mAnalyticsManager = AnalyticsManager.instance;
-
-        // Read in the flag indicating whether or not the user has demonstrated awareness of the
-        // drawer. See PREF_USER_LEARNED_DRAWER for details.
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        mUserLearnedDrawer = sp.getBoolean(PREF_USER_LEARNED_DRAWER, false);
 
         if (savedInstanceState != null) {
             mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
@@ -163,24 +156,9 @@ public class NavigationDrawerFragment extends Fragment {
                     return;
                 }
 
-                if (!mUserLearnedDrawer) {
-                    // The user manually opened the drawer; store this flag to prevent auto-showing
-                    // the navigation drawer automatically in the future.
-                    mUserLearnedDrawer = true;
-                    SharedPreferences sp = PreferenceManager
-                            .getDefaultSharedPreferences(getActivity());
-                    sp.edit().putBoolean(PREF_USER_LEARNED_DRAWER, true).apply();
-                }
-
                 mAnalyticsManager.logEvent(AnalyticsManager.EVENT_MENU_OPENED);
             }
         };
-
-        // If the user hasn't 'learned' about the drawer, open it to introduce them to the drawer,
-        // per the navigation drawer design guidelines.
-        if (!mUserLearnedDrawer && !mFromSavedInstanceState) {
-            mDrawerLayout.openDrawer(mFragmentContainerView);
-        }
 
         // Defer code dependent on restoration of previous instance state.
         mDrawerLayout.post(new Runnable() {
