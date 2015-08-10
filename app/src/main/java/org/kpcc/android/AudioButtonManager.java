5a33b50bf1108adf225c2a62d6a64a7083efde01
Bryan Ricker <bryancricker@gmail.com>
2015-08-10 01:30:52 -0700

    Remove stop buttons

--- app/src/main/java/org/kpcc/android/AudioButtonManager.java
+++ app/src/main/java/org/kpcc/android/AudioButtonManager.java
@@ -9,51 +9,36 @@ import android.widget.TextView;
 class AudioButtonManager {
     private final ImageButton mPlayButton;
     private final ImageButton mPauseButton;
-    private final ImageButton mStopButton;
     private final ProgressBar mLoadingInd;
     private final TextView mErrorMsg;
 
     public AudioButtonManager(@NonNull View view) {
         mPlayButton = (ImageButton) view.findViewById(R.id.play_button);
         mPauseButton = (ImageButton) view.findViewById(R.id.pause_button);
-        mStopButton = (ImageButton) view.findViewById(R.id.stop_button);
         mLoadingInd = (ProgressBar) view.findViewById(R.id.progress_circular);
         mErrorMsg = (TextView) view.findViewById(R.id.error_message);
     }
 
     public void toggleLoading() {
-        mStopButton.setVisibility(View.GONE);
         mPauseButton.setVisibility(View.GONE);
         mLoadingInd.setVisibility(View.VISIBLE);
         mPlayButton.setVisibility(View.GONE);
     }
 
     public void togglePlayingForPause() {
-        mStopButton.setVisibility(View.GONE);
         mPauseButton.setVisibility(View.VISIBLE);
         mLoadingInd.setVisibility(View.GONE);
         mPlayButton.setVisibility(View.GONE);
     }
 
-    public void togglePlayingForStop() {
-        mStopButton.setVisibility(View.VISIBLE);
-        mPauseButton.setVisibility(View.GONE);
-        mLoadingInd.setVisibility(View.GONE);
-        mPlayButton.setVisibility(View.GONE);
-    }
-
     public void togglePaused() {
-        mStopButton.setVisibility(View.GONE);
         mPauseButton.setVisibility(View.GONE);
         mLoadingInd.setVisibility(View.GONE);
         mPlayButton.setVisibility(View.VISIBLE);
     }
 
     public void toggleStopped() {
-        mStopButton.setVisibility(View.GONE);
-        mPauseButton.setVisibility(View.GONE);
-        mLoadingInd.setVisibility(View.GONE);
-        mPlayButton.setVisibility(View.VISIBLE);
+        togglePaused();
     }
 
     // These are a way to just manage the error message, because sometimes there are
@@ -74,10 +59,6 @@ class AudioButtonManager {
 
     public ImageButton getPauseButton() {
         return mPauseButton;
-    }
-
-    public ImageButton getStopButton() {
-        return mStopButton;
     }
 
     public void clickPlay() {
