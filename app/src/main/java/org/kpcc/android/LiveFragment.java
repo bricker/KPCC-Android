95d5b06d11a9f2617000054f83aa4b5fbbf82a03
Bryan Ricker <bryancricker@gmail.com>
2016-04-19 01:09:35 -0700

    Cleanup

--- app/src/main/java/org/kpcc/android/LiveFragment.java
+++ app/src/main/java/org/kpcc/android/LiveFragment.java
@@ -1,6 +1,5 @@
 package org.kpcc.android;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -9,7 +8,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.NotificationBuilderWithBuilderAccessor;
 import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
